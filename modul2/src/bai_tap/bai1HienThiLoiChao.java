ce9da68236c53b31e2e2a65c1bf552f02ab0f21f
unknown <anhthuphan0102@gmail.com>
2023-06-14 19:36:19 +0700

    nop bai

---
+++ modul2/src/bai_tap/bai1HienThiLoiChao.java
@@ -0,0 +1,13 @@
+package bai_tap;
+
+import java.util.Scanner;
+
+public class bai1HienThiLoiChao {
+    public static void main(String[] args) {
+        System.out.println(" Enter your name: ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        System.out.println(" Hello: " + name);
+    }
+}
